01c60d7e4c0aced6863b7b6f63c0578b624f4066
casonbobo <casonsbobo@gmail.com>
2023-12-20 22:03:17 -0600

    Math worky

--- test_js.js
+++ test_js.js
@@ -1,5 +1,5 @@
 // Get the div element
-var divElement = document.getElementById('scene-5');
+var divElement = document.getElementById('gr2');
 
 // Create an image element
 var imgElement = document.createElement("img");
@@ -8,19 +8,15 @@ var imgElement = document.createElement("img");
 imgElement.src = 'images/tree_1.png';
 
 // Generate random position
-// var randomTop = Math.random() * divElement.offsetHeight;
-// var randomLeft = Math.random() * divElement.offsetWidth;
+var randomTop = Math.random() * (divElement.offsetHeight - imgElement.offsetHeight);
+var randomLeft = Math.random() * (divElement.offsetWidth - imgElement.offsetWidth);
 
 // Set the image position
-// imgElement.style.top = randomTop + 'px';
-// imgElement.style.left = randomLeft + 'px';
+imgElement.style.bottom = randomTop + 'px';
+imgElement.style.left = randomLeft + 'px';
 
 // Set the image position to absolute
 imgElement.style.position = 'absolute';
 
 // Append the image to the div
 divElement.appendChild(imgElement);
-
-// $("#scene-5").replaceWith(`
-//         <img src="images/tree_1.png" class="just-tree1 obj" id="just-tree1">
-// `)
